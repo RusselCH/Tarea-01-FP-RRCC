8493aa265f63cb141951335a93e81a738d1ce1d1
rodrigochaina327@gmail.com <RusselCH>
2022-04-28 09:36:11 -0500

    raaaa

---
+++ ejerciciosjava.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+
+/**
+ * ejercicios java
+ */
+public class ejerciciosjava {
+ public static void main(String[] args) {
+ //2.15    
+//definir variables
+double p, r=4, i;
+//datos de entrada 
+Scanner  lt=new Scanner(System.in);
+System.out.println("ingrese  valor de la intencidad");
+i=lt.nextDouble();
+//proceso
+p =r*Math.pow(i, 2);
+//datos de salida
+System .out.println("la potencia es:"+p );
+ }
+
+ }
+ 
