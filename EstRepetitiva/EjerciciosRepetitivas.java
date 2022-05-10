4c6e10684e0e6d4f43306619f9e60b581a4c5a72
RusselCH <rodrigochaina327@gmail.com>
2022-05-10 17:56:25 +0000

    Holaaaaaaa

---
+++ EstRepetitiva/EjerciciosRepetitivas.java
@@ -0,0 +1,21 @@
+package EstRepetitiva;
+
+public class EjerciciosRepetitivas {
+
+    public static void ejer41while() {
+        int contador=1;
+        int suma=0;
+       while (contador<=20) {
+            if (contador%2==0) {
+                suma=suma+contador; 
+            }
+            contador++;  
+        }
+        System.out.println("la respuesta es:"+suma);  
+       
+    }
+    public static void main(String[] args) {
+         ejer41while();
+     }
+       
+}
