431a5c2a4e23d7e9b0688b1b5fcbdd6980460e18
RusselCH <rodrigochaina327@gmail.com>
2022-05-19 15:00:39 +0000

    repetitiva

---
+++ EstRepetitiva/EjercicioVarios.java
@@ -0,0 +1,14 @@
+package EstRepetitiva;
+
+import java.util.Scanner;
+
+public class EjercicioVarios {
+    
+    static Scanner leerT=new Scanner(System.in)
+
+    public static void main(String[] args) {
+
+        
+    }
+    
+}
